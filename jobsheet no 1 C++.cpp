ee2aabd86cc646423bd114b9cdfc8d06a16393d3
AuliaWidyaPrastuti12 <65170959+AuliaWidyaPrastuti12@users.noreply.github.com>
2020-05-18 09:27:32 +0700

    Add files via upload

---
+++ jobsheet no 1 C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main ()
+{ 
+	string nim_ray[4]={"F1B019031","F1B019017","F1B019019","F1B019016"};
+	cout <<"nim mahasiswa ke- 1 = "<<nim_ray[0]<<"\n";
+	cout <<"nim mahasiswa ke- 2 = "<<nim_ray[1]<<"\n";
+	cout <<"nim mahasiswa ke- 3 = "<<nim_ray[2]<<"\n";
+	cout <<"nim mahasiswa ke- 4 = "<<nim_ray[3]<<"\n";
+	
+}
+
