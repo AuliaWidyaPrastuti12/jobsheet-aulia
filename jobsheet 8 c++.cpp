b6a16789bebf7c761b52bd6e95d53a3438da6f12
AuliaWidyaPrastuti12 <65170959+AuliaWidyaPrastuti12@users.noreply.github.com>
2020-05-18 09:42:01 +0700

    Create jobsheet 8 c++.cpp
    Buatlah program mengisi nilai array dan menampilkannya dengan perulangan dengan index array dan batas pengulangan menggunakan NIM terakhir setiap mahasiswa.

---
+++ jobsheet 8 c++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main (){
+int nim [1];
+for (int i = 0; i <= 1; i++){
+cout << "Masukkan nim ke-" << i+1 << "=";
+cin >> nim[i];
+}
+for (int i = 0; i <= 1; i++){
+cout << "nim ke- " << i+1 << "=" << nim [i] << "\n";
+	}
+}
